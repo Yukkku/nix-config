93cacc533d710908981b225ece519146637de5da
Yukkku <yukku.scratcher@gmail.com>
2026-04-09 11:07:40 +0900

    一旦Neovimの設定をリセット

--- home/yukkku/neovim/default.nix
+++ home/yukkku/neovim/default.nix
@@ -1,19 +1,7 @@
-{ pkgs, ... }:
-{
+_: {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
-    coc.enable = true;
-    plugins = [ pkgs.vimPlugins.vim-airline ];
-    extraConfig = ''
-      set number
-      colorscheme zaibatsu
-      highlight Normal guibg=None
-      highlight EndOfBuffer guibg=None
-      set clipboard+=unnamedplus
-      noremap  <silent> <C-S> :update<CR>
-      vnoremap <silent> <C-S> <C-C>:update<CR><Esc>
-      inoremap <silent> <C-S> <C-O>:update<CR><Esc>
-    '';
+    initLua = "";
   };
 }
