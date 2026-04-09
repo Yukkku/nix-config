d36f6b9cc05fbef87f1e7fe2d8010b33897f110e
Yukkku <yukku.scratcher@gmail.com>
2026-04-09 12:47:56 +0900

    Ctrl+Sで保存できるようにした

--- home/yukkku/neovim/default.nix
+++ home/yukkku/neovim/default.nix
@@ -7,6 +7,10 @@ _: {
       vim.opt.completeopt = { "menu", "menuone", "noselect" }
       vim.opt.clipboard:append({"unnamedplus"})
 
+      vim.keymap.set("n", "<C-s>", ":w<CR>")
+      vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>")
+      vim.keymap.set("v", "<C-s>", "<Esc>:w<CR>")
+
       local local_config = vim.fn.getcwd() .. "/.vim/workspace.lua"
       if vim.fn.filereadable(local_config) == 1 then
         dofile(local_config)
