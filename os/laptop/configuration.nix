e5adaac218f970a57102ff24c30f47db313165f8
Yukkku <yukku.scratcher@gmail.com>
2026-05-11 20:21:35 +0900

    指紋認証を導入

--- os/laptop/configuration.nix
+++ os/laptop/configuration.nix
@@ -1,4 +1,5 @@
-_: {
+{ pkgs, lib, ... }:
+{
   imports = [ ./hardware-configuration.nix ];
 
   nix.settings.experimental-features = [
@@ -33,6 +34,22 @@ _: {
   security.pam.services.swaylock = { };
   programs.dconf.enable = true;
 
+  services.fprintd = {
+    enable = true;
+    tod = {
+      enable = true;
+      driver = pkgs.libfprint-2-tod1-goodix;
+    };
+  };
+
+  security.pam.services.polkit-1.fprintAuth = true;
+  services.udev.packages = [ pkgs.libfido2 ];
+
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "libfprint-2-tod1-goodix"
+    ];
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "25.11";
 }
