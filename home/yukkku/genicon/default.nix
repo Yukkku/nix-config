9417b01b3b3d73d2e4c0f5ba5c633e2ec32fd689
Yukkku <yukku.scratcher@gmail.com>
2026-07-01 22:34:32 +0900

    なんか警告出てたので修正

--- home/yukkku/genicon/default.nix
+++ home/yukkku/genicon/default.nix
@@ -1,6 +1,6 @@
 { inputs, pkgs, ... }:
 let
-  yukkku-genicon = inputs.yukkku-genicon.packages.${pkgs.system}.default;
+  yukkku-genicon = inputs.yukkku-genicon.packages.${pkgs.stdenv.hostPlatform.system}.default;
 in
 {
   home.packages = [ yukkku-genicon ];
