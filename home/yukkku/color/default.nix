eafcf8d5fafce41bc031d137594178236decccb4
Yukkku <yukku.scratcher@gmail.com>
2026-07-20 11:32:06 +0900

    配色の設定を統合的に管理したいので, 落とす

--- home/yukkku/color/default.nix
+++ home/yukkku/color/default.nix
@@ -1,6 +1,6 @@
 { ... }:
 let
-  makeColor = r: g: b: rec {
+  mkColor = r: g: b: rec {
     inherit r g b;
     hex =
       let
@@ -13,14 +13,16 @@ let
   };
 in
 rec {
-  black = makeColor 34 34 34;
-  red = makeColor 234 71 71;
-  green = makeColor 33 166 33;
-  yellow = makeColor 146 146 0;
-  blue = makeColor 117 117 255;
-  magenta = makeColor 198 83 198;
-  cyan = makeColor 0 172 172;
-  white = makeColor 255 255 255;
+  black = mkColor 34 34 34;
+  white = mkColor 255 255 255;
+
+  red = mkColor 245 101 90;
+  orange = mkColor 237 147 63;
+  yellow = mkColor 219 196 52;
+  green = mkColor 80 214 93;
+  cyan = mkColor 33 213 228;
+  blue = mkColor 101 119 252;
+  magenta = mkColor 229 102 224;
 
   info = green;
   warn = yellow;
