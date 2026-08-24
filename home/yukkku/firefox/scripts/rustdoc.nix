8b573d298741e1dfa93f95e65756e1c2662a800c
Yukkku <yukku.scratcher@gmail.com>
2026-08-24 11:08:32 +0900

    漉すぎかな...

--- home/yukkku/firefox/scripts/rustdoc.nix
+++ home/yukkku/firefox/scripts/rustdoc.nix
@@ -4,7 +4,7 @@
     :root:has(rustdoc-topbar) {
       --main-background-color: transparent !important;
       --sidebar-background-color: transparent !important;
-      --code-block-background-color: rgba(0 0 0 / 0.5) !important;
+      --code-block-background-color: rgba(0 0 0 / 0.3) !important;
       & .docblock code {
         background-color: transparent !important;
       }
