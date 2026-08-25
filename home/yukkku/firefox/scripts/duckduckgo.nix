7907b6398c4588a409b0096245b4754c185ad780
Yukkku <yukku.scratcher@gmail.com>
2026-08-25 18:13:51 +0900

    透明度up

--- home/yukkku/firefox/scripts/duckduckgo.nix
+++ home/yukkku/firefox/scripts/duckduckgo.nix
@@ -6,6 +6,9 @@
       --sds-color-background-01: transparent !important;
       --theme-col-bg-page: transparent !important;
       --theme-col-bg-search: rgba(0 0 0 / 0.5) !important;
+      --theme-col-bg-card: rgba(0 0 0 / 0.3) !important;
+      --theme-col-border-ui: rgba(0 0 0 / 0.3) !important;
+      --sds-color-background-container-01: rgba(0 0 0 / 0.3) !important;
     }
     body, .site-wrapper, .header-wrap {
       background-color: transparent !important;
