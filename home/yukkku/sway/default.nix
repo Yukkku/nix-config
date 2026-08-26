86af3fc076d4e35dfae8f677c1a6a018d502ccb0
Yukkku <yukku.scratcher@gmail.com>
2026-08-26 16:37:51 +0900

    Swayのキーバインドをちゃんと管理したい

--- home/yukkku/sway/default.nix
+++ home/yukkku/sway/default.nix
@@ -33,14 +33,76 @@
         "k" = "resize shrink height 16 px";
         "l" = "resize grow width 16 px";
       };
-      keybindings = lib.mkOptionDefault {
-        "Mod4+x" = "exec ${pkgs.shotman}/bin/shotman -c output";
-        "Mod4+c" = "exec ${pkgs.shotman}/bin/shotman -c region";
-        "Mod4+Shift+c" = "exec ${pkgs.shotman}/bin/shotman -c window";
-      };
-      output."*".bg = "${./bg4.jpg} fill";
 
       terminal = lib.getExe config.programs.foot.package;
+
+      keybindings =
+        let
+          cfg = config.wayland.windowManager.sway.config;
+          mod = cfg.modifier;
+        in
+        {
+          "${mod}+Return" = "exec ${cfg.terminal}";
+          "${mod}+Shift+q" = "kill";
+          "${mod}+d" = "exec ${cfg.menu}";
+
+          "${mod}+${cfg.left}" = "focus left";
+          "${mod}+${cfg.down}" = "focus down";
+          "${mod}+${cfg.up}" = "focus up";
+          "${mod}+${cfg.right}" = "focus right";
+
+          "${mod}+Left" = "focus left";
+          "${mod}+Down" = "focus down";
+          "${mod}+Up" = "focus up";
+          "${mod}+Right" = "focus right";
+
+          "${mod}+Shift+${cfg.left}" = "move left";
+          "${mod}+Shift+${cfg.down}" = "move down";
+          "${mod}+Shift+${cfg.up}" = "move up";
+          "${mod}+Shift+${cfg.right}" = "move right";
+
+          "${mod}+Shift+Left" = "move left";
+          "${mod}+Shift+Down" = "move down";
+          "${mod}+Shift+Up" = "move up";
+          "${mod}+Shift+Right" = "move right";
+
+          "${mod}+f" = "fullscreen toggle";
+          "${mod}+a" = "focus parent";
+
+          "${mod}+Shift+space" = "floating toggle";
+          "${mod}+space" = "focus mode_toggle";
+
+          "${mod}+1" = "workspace number 1";
+          "${mod}+2" = "workspace number 2";
+          "${mod}+3" = "workspace number 3";
+          "${mod}+4" = "workspace number 4";
+          "${mod}+5" = "workspace number 5";
+          "${mod}+6" = "workspace number 6";
+          "${mod}+7" = "workspace number 7";
+          "${mod}+8" = "workspace number 8";
+          "${mod}+9" = "workspace number 9";
+          "${mod}+0" = "workspace number 10";
+
+          "${mod}+Shift+1" = "move container to workspace number 1";
+          "${mod}+Shift+2" = "move container to workspace number 2";
+          "${mod}+Shift+3" = "move container to workspace number 3";
+          "${mod}+Shift+4" = "move container to workspace number 4";
+          "${mod}+Shift+5" = "move container to workspace number 5";
+          "${mod}+Shift+6" = "move container to workspace number 6";
+          "${mod}+Shift+7" = "move container to workspace number 7";
+          "${mod}+Shift+8" = "move container to workspace number 8";
+          "${mod}+Shift+9" = "move container to workspace number 9";
+          "${mod}+Shift+0" = "move container to workspace number 10";
+
+          "${mod}+Shift+e" = "exec swaymsg exit";
+
+          "${mod}+x" = "exec ${pkgs.shotman}/bin/shotman -c output";
+          "${mod}+c" = "exec ${pkgs.shotman}/bin/shotman -c region";
+          "${mod}+Shift+c" = "exec ${pkgs.shotman}/bin/shotman -c window";
+        };
+
+      output."*".bg = "${./bg4.jpg} fill";
+
     };
   };
 
