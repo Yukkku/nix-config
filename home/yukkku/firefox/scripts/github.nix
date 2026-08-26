d62d2cad2ef77fa55f3674e8702ff3a7d1ca46e9
Yukkku <yukku.scratcher@gmail.com>
2026-08-26 19:04:12 +0900

    なんか表示が気に食わなかったので修正

--- home/yukkku/firefox/scripts/github.nix
+++ home/yukkku/firefox/scripts/github.nix
@@ -4,8 +4,14 @@
     :root {
       --bgColor-default: transparent !important;
       --bgColor-inset: transparent !important;
-      --bgColor-muted: rgba(0 0 0 / 0.5) !important;
+      --bgColor-muted: rgba(0 0 0 / 0.3) !important;
       --overlay-bgColor: rgba(0 0 0 / 0.5) !important;
+      --button-default-bgColor-rest: rgba(0 0 0 / 0.3) !important;
+      --button-default-bgColor-hover: rgba(0 0 0 / 0.5) !important;
+      --button-default-bgColor-active: rgba(0 0 0 / 0.5) !important;
+    }
+    .markdown-body pre {
+      background-color: transparent !important;
     }
   '';
 }
