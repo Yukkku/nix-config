26c3f4de25a16563548b89c6b70d327e3027e1df
Yukkku <yukku.scratcher@gmail.com>
2026-09-23 16:52:59 +0900

    RustDocの色を改善

--- home/yukkku/firefox/scripts/rustdoc.nix
+++ home/yukkku/firefox/scripts/rustdoc.nix
@@ -3,20 +3,30 @@
   css = writeText "custom.css" ''
     :root:has(rustdoc-topbar, rustdoc-toolbar) {
       --main-background-color: transparent !important;
-      --sidebar-background-color: transparent !important;
-      --code-block-background-color: rgba(0 0 0 / 0.3) !important;
-      --stab-background-color: rgba(0 0 0 / 0.3) !important;
-      --src-sidebar-background-selected: rgba(0 0 0 / 0.3) !important;
-      --sidebar-current-link-background-color: rgba(0 0 0 / 0.3) !important;
       --button-background-color: rgba(0 0 0 / 0.3) !important;
+      --code-block-background-color: rgba(0 0 0 / 0.3) !important;
+
       --search-color: #fff !important;
-      --search-tab-button-not-selected-background: rgba(0 0 0 / 0.3) !important;
       --search-tab-button-selected-background: rgba(0 0 0 / 0.5) !important;
-      & .docblock code {
+      --search-tab-button-not-selected-background: rgba(0 0 0 / 0.3) !important;
+
+      --sidebar-background-color: transparent !important;
+      --sidebar-current-link-background-color: rgba(0 0 0 / 0.3) !important;
+
+      --src-sidebar-background-hover: rgba(0 0 0 / 0.3) !important;
+      --src-sidebar-background-selected: rgba(0 0 0 / 0.3) !important;
+
+      --stab-background-color: rgba(0 0 0 / 0.3) !important;
+      --target-background-color: rgba(0 0 0 / 0.3) !important;
+
+      & pre code {
         background-color: transparent !important;
       }
       & .sidebar .current {
-         background-color: transparent !important;
+        background-color: transparent !important;
+      }
+      & rustdoc-toolbar {
+        --main-background-color: rgba(0 0 0 / 0.5) !important;
       }
     }
   '';
